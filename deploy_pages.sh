f53069cc3a18f8d94bd1aeba79aa9fc878f22466
ian <chiknrice@gmail.com>
2015-08-31 01:04:46 +1200

    updated script to be more intelligent

--- deploy_pages.sh
+++ deploy_pages.sh
@@ -10,23 +10,27 @@ cd gh-pages
 
 git clone --depth=50 --branch=gh-pages https://$GH_TOKEN@github.com/chiknrice/concordion-assert-extension.git
 
-VER="${TRAVIS_TAG:-latest}"
-
-if [ $VER == "latest" ];
+if [ -v TRAVIS_TAG ]
 then
-    rm -rf concordion-assert-extension/$VER
+    echo "Updating concordion specs for $TRAVIS_TAG"
+    mkdir concordion-assert-extension/$TRAVIS_TAG
+    cp -R ../build/reports/spec/* concordion-assert-extension/$TRAVIS_TAG
 fi
 
-mkdir concordion-assert-extension/$VER
+if [ -n $(git status -s) ]
+then
+    echo "Updating latest concordion specs"
+    rm -rf concordion-assert-extension/latest
+    mkdir concordion-assert-extension/latest
+    cp -R ../build/reports/spec/* concordion-assert-extension/latest
 
-cp -R ../build/reports/spec/* concordion-assert-extension/$VER
+    cd concordion-assert-extension
 
-cd concordion-assert-extension
+    git config user.email "chiknrice@gmail.com"
+    git config user.name "Travis CI"
 
-git config user.email "chiknrice@gmail.com"
-git config user.name "Travis CI"
+    git add .
+    git commit -m "Update concordion spec result"
 
-git add .
-git commit -m "Update concordion spec result"
-
-git push
+    git push
+fi
